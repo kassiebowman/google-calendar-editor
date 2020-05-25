623bdf02db9dc4ebbecc1f88222c022de2487606
Kassie M. Bowman <19256422+kassiebowman@users.noreply.github.com>
2020-05-25 12:44:40 -0700

    Add Google authentication using Spring boot

--- src/main/java/dev/kassie/calendar/MainApplication.java
+++ src/main/java/dev/kassie/calendar/MainApplication.java
@@ -2,26 +2,60 @@ package dev.kassie.calendar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.security.GeneralSecurityException;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
-public class MainApplication
+public class MainApplication extends WebSecurityConfigurerAdapter
 {
-    public static void main(String[] args) throws IOException, GeneralSecurityException
+    @SuppressWarnings("unused")
+    @GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal)
     {
-        SpringApplication.run(MainApplication.class, args);
-
-        CalendarEditor.start(args);
+        return Collections.singletonMap("name", principal.getAttribute("name"));
     }
 
-    @GetMapping("/")
-    public String hello()
+    @SuppressWarnings("unused")
+    @GetMapping("/error")
+    public String error(HttpServletRequest request)
     {
-        return "Hello world!";
+        String message = (String) request.getSession().getAttribute("error.message");
+        request.getSession().removeAttribute("error.message");
+        return message;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        SimpleUrlAuthenticationFailureHandler handler = new SimpleUrlAuthenticationFailureHandler("/");
+
+        http.authorizeRequests(a -> a.antMatchers("/", "/error", "/webjars/**").permitAll().anyRequest().authenticated())
+                .exceptionHandling(e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+                .csrf(c -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .logout(l -> l.logoutSuccessUrl("/").permitAll())
+                .oauth2Login(o -> o
+                        .failureHandler((request, response, exception) -> {
+                            request.getSession().setAttribute("error.message", exception.getMessage());
+                            handler.onAuthenticationFailure(request, response, exception);
+                        })
+                );
+    }
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MainApplication.class, args);
     }
 }
