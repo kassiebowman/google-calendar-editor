685309eec92a6786b1dc4e24a7d7f587ab735003
Kassie Bowman <kbowman@ctic.us>
2022-03-26 11:23:53 -0700

    Update dependency versions to latest

--- src/main/java/kmb/calendar/CalendarEditor.java
+++ src/main/java/kmb/calendar/CalendarEditor.java
@@ -8,7 +8,7 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.CalendarScopes;
@@ -40,7 +40,7 @@ public class CalendarEditor
     private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     private static final String APPLICATION_NAME = "Google Calendar Editor";
-    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
     private static final String TOKENS_DIRECTORY_PATH = "tokens";
     private static final long THREE_DAYS_IN_MS = TimeUnit.DAYS.toMillis(3);
 
